762ea2b312a0052d1e8354f6d48edd4a6107ad7d
Jessica de Oliveira <jessica-lm-@hotmail.com>
2021-10-02 11:13:08 -0300

    page-criar/buscar-sala

---
+++ src/pages/CreateRoom.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function CreateRoom() {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <h1>Criar Sala</h1>
+            </div>
+        </div>
+    )
+}
+
+export default CreateRoom
---
+++ src/pages/SearchRoom.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function SearchRoom() {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <h1>Buscar Sala</h1>
+            </div>
+        </div>
+    )
+}
+
+export default SearchRoom
