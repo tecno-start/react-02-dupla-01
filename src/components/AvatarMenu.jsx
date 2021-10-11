18910cbd89c7f732c4b7cf0288ebcb18641adb7f
Jessica de Oliveira <jessica-lm-@hotmail.com>
2021-10-11 20:23:49 -0300

    correção-usuario-conectado

--- src/components/AvatarMenu.jsx
+++ src/components/AvatarMenu.jsx
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 import './AvatarMenu.scss'
 import { LOGIN } from '../routes/routes';
 import { logout } from '../auth/auth';
+import jwt from 'jwt-decode'
 
 
 
 
-function AvatarMenu({ userName, avatarMenu }) {
+function AvatarMenu({ avatarMenu }) {
     return (
         <div className={avatarMenu ? 'menu-container active' : 'menu-container'}>
             <ul className="user-menu">
                 <div className="profile-highlight">
                     <div className="details">
-                        <div id="profile-name">{userName}</div>
+                        <div id="profile-name">{jwt(sessionStorage.getItem("token")).name}</div>
                         <div id="profile-footer">Tecnostart</div>
                     </div>
                 </div>
