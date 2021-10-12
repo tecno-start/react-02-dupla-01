03323351fd4713bc537bd6e99bfc74c87c95f7ec
Jessica de Oliveira <jessica-lm-@hotmail.com>
2021-10-12 14:47:02 -0300

    correcao-rotas

--- src/pages/RedefinePassword.jsx
+++ src/pages/RedefinePassword.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
 import ButtonGoBack from '../components/ButtonGoBack';
 import { Row, Col, Card } from 'reactstrap';
 
 function RedefinePassword() {
     return (
-        <div>
-            <div>
-                <Sidebar />
-            </div>
-            <div>
-            <Row
-                    style={{
-                        height: '50vh',
-                        justifyContent: 'center',
-                        alignItems: 'center',
+        <div className="bg-gradient-theme-left border-0">
+
+            <Row 
+                style={{
+                    height: '50vh',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <Col  md={6} lg={4}>
+                    <Card body style={{
+                        borderRadius: '10px'
                     }}>
-                    <Col md={6} lg={4}>
-                        <Card body style={{
-                            borderRadius: '10px'
-                        }}>
-                            <h1>Redefinir Senha</h1>
-                            <ButtonGoBack/>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
+                        <h1>Redefinir Senha</h1>
+                        <ButtonGoBack />
+                    </Card>
+                </Col>
+            </Row>
+
         </div>
     )
 }
