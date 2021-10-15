15fe6e4c7af4f11b6191ac6bc061b666d3e554de
Gabriel martins <gabrielmartinspsn@gmail.com>
2021-10-14 22:16:50 -0300

    atualiza

--- src/components/ButtonCopy.jsx
+++ src/components/ButtonCopy.jsx
@@ -7,4 +7,10 @@ function myFunction() {
   copyText.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(copyText.value);
   alert("Copied the text: " + copyText.value);
+
+  return (
+    <div>
+        <button type='button' onClick={voltar} className="bg-gradient-theme-left border-0">Voltar</button>
+    </div>
+)
 }
